69ae05d04379fde255b5599bfa9036e3d60301d1
road97 <dlehdud224@gmail.com>
2024-07-05 10:30:24 +0900

    ch01

---
+++ ch01/src/sub1/Hello.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/*
+	날짜 : 2024/06/28
+	이름 : 이도영
+	내용 : ch01. Java 개요및 개발환경 구축
+	main후 컨트롤+스페이스바
+*/
+public class Hello {
+	public static void main(String[] args) {
+		//프로그래밍 입문할 때 상징적으로 출려하는 문장
+		System.out.println("Hello World");
+		
+	}
+}

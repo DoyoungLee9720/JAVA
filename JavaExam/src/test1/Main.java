67795513712a9c044f832e64d27d50339b76a22f
DoyoungLee9720 <dlehdud224@gmail.com>
2024-07-12 17:51:38 +0900

    2024-07-12

--- JavaExam/src/test1/Main.java
+++ JavaExam/src/test1/Main.java
@@ -1,34 +1,24 @@
 package test1;
-import java.util.Arrays;
-import java.io.IOException;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        String S =sc.nextLine();
-        char oneword = 'a';
-        int[] check = new int[26];
-        for(int i=0; i<26;i++) {
-        	check[i] = -1;
-        }
-        for(int i=0; i<S.length();i++) {
-        	for(int j=0;j<26;j++) {
-        		if(oneword == S.charAt(i)) {
-        			if(check[j] == -1) {
-        				check[j] = i;
-        			}
-        		}
-        		oneword++;
-        	}
-        	oneword = 'a';
-        }
-        for(int i=0;i<26;i++) {
-        	System.out.print(check[i] + " ");
+        String F1 = sc.next();
+        String F2 = sc.next();
+        StringBuffer str1 = new StringBuffer(F1);
+        F1 = str1.reverse().toString();
+        
+        StringBuffer str2 = new StringBuffer(F2);
+        F2 = str2.reverse().toString();
+        
+        if(Integer.parseInt(F1)>Integer.parseInt(F2)) {
+        	System.out.println(F1);
+        }else {
+        	System.out.println(F2);
         }
     }
 }
